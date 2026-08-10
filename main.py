c8355cc170bcd992b7e82e8f77a18a4802fb444c
Sanketh B M <sankethbmahesh@gmail.com>
2026-08-10 13:32:52 +0530

    Add drag calculation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.atmosphere import calculate_density, calculate_dynamic_pressure
-from src.aerodynamics import calculate_lift
+from src.aerodynamics import calculate_lift, calculate_drag
 
 altitude = 1000
 velocity = 20
@@ -12,6 +12,10 @@ cl = 0.8
 
 lift = calculate_lift(dynamic_pressure, wing_area, cl)
 
+cd = 0.05
+drag = calculate_drag(dynamic_pressure, wing_area, cd)
+
 print("Density:", density, "kg/m^3")
 print("Dynamic Pressure:", dynamic_pressure, "Pa")
 print("Lift:", lift, "N")
+print("Drag:", drag, "N")
--- src/aerodynamics.py
+++ src/aerodynamics.py
@@ -1,3 +1,8 @@
 def calculate_lift(dynamic_pressure, wing_area, cl):
     lift = dynamic_pressure * wing_area * cl
     return lift
+
+def calculate_drag(dynamic_pressure, wing_area, cd):
+    drag = dynamic_pressure * wing_area * cd
+
+    return drag
